b19e1ee510b0b9472f5a87b3b9905c66a1ce338f
EchoCow <lizhongyue248@163.com>
2019-08-13 14:15:42 +0800

    :tada: Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin("jvm") version "1.3.30"
+  id("io.vertx.vertx-plugin") version "0.8.0"
+}
+
+group = "cn.edu.gzmu"
+version = "1.0.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+  maven ("http://maven.aliyun.com/nexus/content/groups/public/")
+  mavenCentral()
+  jcenter()
+}
+//  kotlinVersion = "1.3.30"
+//  vertxVersion = "3.8.0"
+
+dependencies {
+  implementation("io.vertx:vertx-web-client")
+  implementation("io.vertx:vertx-auth-jwt")
+  implementation("io.vertx:vertx-web")
+  implementation("io.vertx:vertx-mysql-client")
+  implementation("io.vertx:vertx-auth-oauth2")
+  implementation("io.vertx:vertx-lang-kotlin")
+  implementation("io.github.jponge:vertx-boot:1.1.0")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+  testImplementation("io.vertx:vertx-junit5")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+}
+
+vertx {
+  mainVerticle = "io.github.jponge.vertx.boot.BootVerticle"
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "1.8"
+  }
+}
